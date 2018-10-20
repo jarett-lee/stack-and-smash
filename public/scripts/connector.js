8e3914f2dca9cc66dbbcd3ea4bf1504440299dc1
Samuel Ingram <samingram22@gmail.com>
2018-10-20 01:56:48 -0400

    Add Join Game Functionality

--- public/scripts/connector.js
+++ public/scripts/connector.js
@@ -4,25 +4,32 @@
 
 const socket = io('http://localhost:3000');
 
+/**
+ * Chuck the user in a game and get the game token
+ */
 function createGame () {
     // Request to make a game from the server
     socket.emit('create-game', function (token) {
         // Receive game token from server upon successful request
         document.getElementById("game-token").innerHTML = token;
-
         show('game-id');
     });
 }
 
+/**
+ * Request to join a game with the token in the token field
+ */
 function joinGame () {
     const gameToken = document.getElementById("game-token-input").value;
 
-    // Send the game token to the server or something
-
-    // If successfully joined game from game token
-    if (success) {
-        show("game");
-    } else {
-        // Alert that the token was not right
-    }
+    // Send the game token to the server
+    socket.emit('join-game', gameToken, function (success) {
+        // If successfully joined game from game token
+        if (success) {
+            show("game");
+        } else {
+            // Alert that the token was not right
+            alert("The token entered is invalid!");
+        }
+    });
 }
