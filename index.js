3eecc844440e857c3db16a84ace8c158e5b1cc76
Joey Vinyard <jviny98@yahoo.com>
2018-10-19 22:33:38 -0400

    Express framework

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+    console.log("Listening on port", port);
+})
