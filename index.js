7b7ce83f86911911516ab141753a16870032e277
Joey Vinyard <jviny98@yahoo.com>
2018-10-19 23:10:44 -0400

    Sends index.html and css/js files

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const port = process.env.port || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.sendFile("/", {root: 'public'});
+});
+
+app.get('/*.*', (req, res) => {
+    res.sendFile(`${req.params[0]}.${req.params[1]}`, { root: 'public' });
+})
 
 app.listen(port, () => {
     console.log("Listening on port", port);
-})
+});
