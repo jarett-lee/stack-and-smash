5b02101df4299e29c6237c457cf8418dc70c90eb
Calvin Henry <calvinhenry60@gmail.com>
2018-10-20 00:39:42 -0400

    reorganized engine and sockets

--- engine.js
+++ engine.js
@@ -1,13 +1,8 @@
 const p2  = require('p2');
-let tokengen = require('rand-token');
+
 
 let worlds = {};
 
 function initWorld(){
-
-    let tok = "";
-    while(!((tok = tokengen.generate(4)) in worlds));//Generate that doesn't already exist
-    worlds[tok] = new p2.World();
     
-    return tok;
 }
--- sockets.js
+++ sockets.js
@@ -1,12 +1,16 @@
 
+   
 module.exports = (server) => {
     const io = require('socket.io')(server);
-    
+    let tokengen = require('rand-token');
+    let tokens = [];
     io.on('connection', (socket) => {
 
         socket.on('create-game', () => {
-            
-            
+            let tok = "";
+            while(!((tok = tokengen.generate(4)) in tokens));//Generate that doesn't already exist
+            tokens.push(tok);
+            return tok;
         })
         console.log("connection!");
     });
