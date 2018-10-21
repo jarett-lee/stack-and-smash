c62ef7ae5d8fe361d2e96f6073bbf4b1189b5af1
Joey Vinyard <jviny98@yahoo.com>
2018-10-21 04:34:43 -0400

    real deploy

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.get('/*.*', (req, res) => {
     res.sendFile(`${req.params[0]}.${req.params[1]}`, { root: 'public' });
 })
 
-server.listen(process.env.port || 3000, () => {
-    console.log("Listening on port", process.env.port || 3000);
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Listening on port", process.env.PORT || 3000);
 });
