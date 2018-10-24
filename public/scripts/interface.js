084ccdf79ab9d87a0905dac50ca8a259bf5f19bd
jarett-lee <jarett.lee.pi@gmail.com>
2018-10-24 19:58:56 -0400

    Remove broken double touch code

--- public/scripts/interface.js
+++ public/scripts/interface.js
@@ -8,12 +8,3 @@ function show (id) {
 
     shownScreen = id;
 }
-
-var doubleTouchStartTimestamp = 0;
-document.addEventListener("touchstart", function(event){
-    var now = +(new Date());
-    if (doubleTouchStartTimestamp + 500 > now){
-        event.preventDefault();
-    };
-    doubleTouchStartTimestamp = now;
-});
